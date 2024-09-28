16319637875062b43ba08cc7d9a946d5566eeb38
Leo Ware <leoware@uni.minerva.edu>
2024-09-28 09:50:20 -0400

    fixed distutils issue

--- torchviz/dot.py
+++ torchviz/dot.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
-from distutils.version import LooseVersion
+# from distutils.version import LooseVersion
+from looseversion import LooseVersion
 from graphviz import Digraph
 import torch
 from torch.autograd import Variable
